fe26b80401e38f276b9ac84914c828dae4051624
Alberto Poblacion <alberto.poblacion@travelportdigital.com>
2017-11-06 19:41:55 +0000

    * Added exo player in StepDetailFragment to show the video * Update gradle version pluging

--- app/src/main/java/com/pobla/baking/ui/recipe/view/StepDetailFragment.java
+++ app/src/main/java/com/pobla/baking/ui/recipe/view/StepDetailFragment.java
@@ -1,15 +1,29 @@
 package com.pobla.baking.ui.recipe.view;
 
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 import com.pobla.baking.R;
 import com.pobla.baking.data.storage.db.StepColumns;
 import com.pobla.baking.ui.recipe.RecipeStepsListActivity;
@@ -52,7 +66,8 @@ public class StepDetailFragment extends Fragment implements StepDetailView {
   FloatingActionButton fabNext;
   @BindView(R.id.recipe_detail_button_back)
   FloatingActionButton fabBack;
-
+  @BindView(R.id.recipe_detail_video_player)
+  SimpleExoPlayerView videoPlayerView;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -83,9 +98,25 @@ public class StepDetailFragment extends Fragment implements StepDetailView {
     if (cursor != null && cursor.moveToNext()) {
       getActivity().setTitle(Cursors.getString(cursor, StepColumns.SHORT_DESCRIPTION));
       textViewDescription.setText(Cursors.getString(cursor, StepColumns.DESCRIPTION));
+      //TODO fix view when video is gone (description appears half way there
+      //TODO show media player full screen when phone horizontal
+      //TODO fix vide doesn't show if controllers are in the screen
+      String videoUrl = Cursors.getString(cursor, StepColumns.VIDEO_URL);
+      if(!TextUtils.isEmpty(videoUrl)){
+        videoPlayerView.setVisibility(View.VISIBLE);
+        play(Uri.parse(videoUrl));
+      }else{
+        stopPlayer();
+        videoPlayerView.setVisibility(View.GONE);
+      }
     }
   }
 
+  @Override
+  public void onPause() {
+    super.onPause();
+    stopPlayer();
+  }
 
   @OnClick(R.id.recipe_detail_button_back)
   public void previousStep() {
@@ -106,6 +137,33 @@ public class StepDetailFragment extends Fragment implements StepDetailView {
   @Override
   public void showNext(boolean show) {
     fabNext.setVisibility(show ? View.VISIBLE : View.GONE);
+  }
+
+  private void play(Uri mediaUri) {
+    initilisePlayerIfRequired();
+
+    String userAgent = Util.getUserAgent(this.getContext(), getString(R.string.app_name));
+    MediaSource mediaSource = new ExtractorMediaSource(mediaUri, new DefaultDataSourceFactory(this.getContext(), userAgent), new DefaultExtractorsFactory(), null, null);
+    SimpleExoPlayer player = videoPlayerView.getPlayer();
+    player.prepare(mediaSource);
+    player.setPlayWhenReady(true);
+  }
+
+  private void initilisePlayerIfRequired() {
+    if (videoPlayerView.getPlayer() == null) {
+      TrackSelector trackSelector = new DefaultTrackSelector();
+      LoadControl loadControl = new DefaultLoadControl();
+      SimpleExoPlayer videoPlayer = ExoPlayerFactory.newSimpleInstance(this.getContext(), trackSelector, loadControl);
+      videoPlayerView.setPlayer(videoPlayer);
+    }
+  }
+
+
+  private void stopPlayer() {
+    SimpleExoPlayer player = videoPlayerView.getPlayer();
+    if(player != null){
+      player.stop();
+    }
   }
 
 }
